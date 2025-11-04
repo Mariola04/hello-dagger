a34de960096ac9accb54c4980e4083e2fdf7d0f4
Dagger <noreply@dagger.io>
2025-11-04 11:04:52 +0000

    change_the_backgroud_1762254276

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main style="background-color: purple;">
+  <main style="background-color: pink;">
     <TheWelcome />
   </main>
 </template>
